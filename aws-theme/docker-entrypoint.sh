9d489c23a18592e7c99fb654726ee9a4fcc4385b
Evo Stamatov <aviolito@gmail.com>
2016-12-16 15:35:56 +1100

    add aws-theme

---
+++ aws-theme/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+[ -z "$AWS_DEFAULT_REGION" ] && AWS_DEFAULT_REGION="us-east-1"
+
+if [ -z "$REPO_NAME" ]; then
+	REPO_NAME=$(basename "$REPO")
+elif [ -z "$REPO"]; then
+	REPO="ssh://git-codecommit.$AWS_DEFAULT_REGION.amazonaws.com/v1/repos/$REPO_NAME"
+fi
+
+if [ -z "$REPO_NAME" ]; then
+	>&2 echo "Bad REPO_NAME: '$REPO_NAME'"
+	exit 1
+fi
+
+[ -z "$THEME_DIRNAME" ] && THEME_DIRNAME="$REPO_NAME"
+
+if [ ! -d "/project/wp-content/themes/$THEME_DIRNAME" ]; then
+	git clone "$REPO" "/project/wp-content/themes/$THEME_DIRNAME" \
+	&& ln -s "wp-content/themes/$THEME_DIRNAME/composer.json" /project/composer.json
+fi
+
+exec "$@"
