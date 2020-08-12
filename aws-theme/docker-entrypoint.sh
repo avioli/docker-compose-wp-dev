c817fc3cbc1d0d2ed25032dbe5cb73a68e744518
Evo Stamatov <aviolito@gmail.com>
2020-08-12 11:49:51 +1000

    fix ionata's composer s3 repo support

--- aws-theme/docker-entrypoint.sh
+++ aws-theme/docker-entrypoint.sh
@@ -18,27 +18,33 @@ fi
 [ -z "$THEME_DIRNAME" ] && THEME_DIRNAME="$REPO_NAME"
 
 PROJECT_ROOT=/project
-OUTPATH=$PROJECT_ROOT
-
+THEME_PATH="$THEME_DIRNAME"
 if [ -n "$BASE_PATH" ]; then
-    OUTPATH="$OUTPATH/$BASE_PATH/$THEME_DIRNAME"
-else
-    OUTPATH="$OUTPATH/$THEME_DIRNAME"
+    THEME_PATH="$BASE_PATH/$THEME_PATH"
 fi
+OUT_PATH="$PROJECT_ROOT/$THEME_PATH"
 
-if [ ! -d "$OUTPATH" -o ! -d "$OUTPATH/.git" ]; then
+# Clone repo
+if [ ! -d "$OUT_PATH" -o ! -d "$OUT_PATH/.git" ]; then
     ARGS=
-
     [ "$RECURSIVE" != "0" ] && ARGS="--recursive"
-
-	git clone $ARGS "$REPO" "$OUTPATH"
+	git clone $ARGS "$REPO" "$OUT_PATH"
 fi
 
+# Install composer-aws s3 installer
+if [ "$USE_IONATA_COMPOSER" == "true" ]; then
+    if ! composer global config repositories.composer-aws >/dev/null 2>&1; then
+        composer global config repositories.composer-aws git ssh://git-codecommit.ap-southeast-2.amazonaws.com/v1/repos/composer-plugin-aws
+        composer global require ionata/composer-aws
+    fi
+fi
+
+# Symlink composer file
 if [ -z "$CWD" ]; then
-    COMPOSER_FILE="$OUTPATH/composer.json"
+    COMPOSER_FILE="$THEME_PATH/composer.json"
     test -f "$COMPOSER_FILE" && ln -sf "$COMPOSER_FILE" $PROJECT_ROOT/composer.json
 else
-    cd "$OUTPATH"
+    cd "$OUT_PATH"
 fi
 
 exec "$@"
