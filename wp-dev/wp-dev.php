710b9933759c50f47bcc322418be6ca21f97598e
Evo Stamatov <aviolito@gmail.com>
2020-10-05 15:50:12 +1100

    add support for multiple WP_HOME_REPLACE_FROM via a separator

--- wp-dev/wp-dev.php
+++ wp-dev/wp-dev.php
@@ -46,11 +46,17 @@ if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
 __define('ABSPATH', WP_CORE_DIR . DIRECTORY_SEPARATOR); // NOTE: ABSPATH is usually already defined (in wp-load.php)
 
 // NOTE: Use WP_HOME_REPLACE_FROM variable to replace a _given string_ with WP_HOME in the rendered output
+// NOTE: We support multiple values with a '|' (pipe char) as delimiter
 if (getenv('WP_HOME_REPLACE_FROM')) {
-	__define('WP_HOME_REPLACE_FROM', getenv('WP_HOME_REPLACE_FROM'));
+	$__replace_from = array_filter(explode('|', getenv('WP_HOME_REPLACE_FROM')), 'trim');
+	if (! empty($__replace_from)) {
+		__define('WP_HOME_REPLACE_FROM', $__replace_from);
+	}
+	unset($__replace_from);
 }
 
-if (defined('WP_HOME_REPLACE_FROM') && strlen(WP_HOME_REPLACE_FROM) > 0) {
+if (defined('WP_HOME_REPLACE_FROM')
+	&& (is_array(WP_HOME_REPLACE_FROM) && sizeof(WP_HOME_REPLACE_FROM) > 0 || strlen(WP_HOME_REPLACE_FROM) > 0)) {
 	// NOTE: WordPress calls wp_ob_end_flush_all() when shutting down (in wp-includes/functions.php).
 	ob_start(function($buf = '') {
 		return str_replace(WP_HOME_REPLACE_FROM, WP_HOME, $buf);
